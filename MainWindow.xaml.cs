e5614cba1ed03a3e6d16312e77bce4b1f3c229c0
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2022-03-10 22:07:32 +0100

    Folder explorer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+
 
 namespace PlatformyTechnologiczne
 {
@@ -26,10 +20,104 @@ namespace PlatformyTechnologiczne
             InitializeComponent();
         }
 
+        private void OnExitItemMenuClick(object sender, RoutedEventArgs e)
+        {
+            //execute parent method
+            this.Close();
+        }
+
         private void OnOpenItemMenuClick(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog dialog = new FolderBrowserDialog();
-            dialog.ShowDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result.Equals(System.Windows.Forms.DialogResult.OK)) LoadTreeView(dialog);
+        }
+
+
+        private void LoadTreeView(FolderBrowserDialog dialog)
+        {
+            System.Windows.Controls.TreeView tree = (System.Windows.Controls.TreeView)this.FindName("directoryTree");
+            string rootDirectory = dialog.SelectedPath;
+            string root = Path.GetFileName(rootDirectory);
+
+            TreeViewItem rootItem = new TreeViewItem { Header = root, Tag = rootDirectory };
+            tree.Items.Add(rootItem);
+
+            string[] directories = Directory.GetDirectories(rootDirectory);
+            string[] files = Directory.GetFiles(rootDirectory);
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                string directoryName = Path.GetFileName(directories[i]);
+                string directoryPath = directories[i];
+
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = directoryName,
+                    Tag = directoryPath
+                };
+
+                rootItem.Items.Add(item);
+                LoadFiles(item, directoryPath);
+            }
+
+            TreeViewItem[] items = new TreeViewItem[files.Length];
+            for (int i = 0; i < items.Length; i++)
+            {
+                string fileName = Path.GetFileName(files[i]);
+                string directoryPath = files[i];
+
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = fileName,
+                    Tag = directoryPath
+                };
+
+                rootItem.Items.Add(item);
+            }
+
+        }
+
+        private void LoadFiles(TreeViewItem parent,string path)
+        {
+            if (!Directory.Exists(path)) return;
+            
+            string[] files = Directory.GetFiles(path);
+            string[] directories = Directory.GetDirectories(path);
+
+            if (files.Length == 0 && directories.Length == 0) return;
+
+
+            for (int i = 0; i < directories.Length; i++)
+            {
+                string directoryName = Path.GetFileName(directories[i]);
+                string directoryPath = directories[i];
+
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = directoryName,
+                    Tag = directoryPath
+                };
+
+                parent.Items.Add(item);
+                LoadFiles(item, directoryPath);
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string fileName = Path.GetFileName(files[i]);
+                string directoryPath = files[i];
+
+                TreeViewItem item = new TreeViewItem
+                {
+                    Header = fileName,
+                    Tag = directoryPath
+                };
+
+                parent.Items.Add(item);
+            }
+
+
         }
     }
 }
